e819955308416ca071e76e04547c32cd3da8637c
akimana-viateur <akimanaviateur94@gmail.com>
2023-10-03 16:56:16 +0200

    Exercises

--- Exercise3.java
+++ Exercise3.java
@@ -1,5 +1,11 @@
 class Exercise3 {
     public static void main(String[] args){
-         
+        System.out.print("Enter a number: ");
+        int num = 0;
+        long factorial = 1;
+        for (int i = 1; i <= num; ++i) {
+            factorial *= i;
+        }
+        System.out.printf("The factorial of %d is %d", num, factorial);
     }
 }
--- Exercise4.java
+++ Exercise4.java
@@ -1,5 +1,22 @@
-public class Exercise4 {
-    public static void main(String[] args){
+ class Exercise4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice;
 
+        do {
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            double sum = num1 + num2;
+
+            System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+            System.out.print("Do you want to perform the operation again? (yes/no): ");
+            choice = scanner.next().toLowerCase();
+
+        } while (choice.equals("yes"));
+        scanner.close();
     }
 }
